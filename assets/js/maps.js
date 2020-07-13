451e5d6f5e2a00210e6d679f3923224ad601238a
David Hubber <david.hubber@icloud.com>
2020-07-13 17:24:32 +0000

    change location

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -2,8 +2,8 @@ function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 3,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 512219.4,
+            lng: 04719.1
         }
     });
 
@@ -22,5 +22,5 @@ function initMap() {
         });
     });
 
-    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+    var MarkerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 }
